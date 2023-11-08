04df55dd55c1a06544985dbd7fc1679ec8785c5e
florla <feliciamlbrownwork@gmail.com>
2023-11-08 14:37:42 -0500

    exercise 1&2

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 //Exercise 1
 function createHelloWorld () {
-    return function () {
-        return "Hello World!";
-    };
+        return "Hello World!";  
 }
 const hello = createHelloWorld();  
 console.log(hello);
 
 //Exercise 2
+const arr = [1,2,3,4];
+const result = [];
+
+
+function transform(x) {
+    return x + 1;
+};
+
+for (let i=0; i < arr.length; i++) {
+    result.push(transform(arr[i]));
+}
+
+console.log(result);
+
+//Exercise 3 
