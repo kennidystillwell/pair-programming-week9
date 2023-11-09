74718ccf36a96514262b8fd6ff90d4d8a6479260
bevans14 <146108421+bevans14@users.noreply.github.com>
2023-11-09 13:39:34 -0500

    commit

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ const hello = createHelloWorld();
 console.log(hello);
 
 //Exercise 2
+
+const arr = [1,2,3,4];
+const result = [];
+
+function transform(x) {
+    return x + 1;
+};
+
+for (let i=0; i < arr.length; i++) {
+    result.push(transform(arr[i]));
+}
+
+console.log(result);
+
