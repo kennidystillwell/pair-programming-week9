539f99e7d86f08890231e9b3f5c123e10d213b48
florla <feliciamlbrownwork@gmail.com>
2023-11-10 15:41:46 -0500

    exercise 6

--- script.js
+++ script.js
@@ -95,3 +95,33 @@ class Bookshelf {
   bookshelf.removeBook({title : "Pride and Prejudice"});
   console.log(bookshelf.getTotalBooks()); // 2
   bookshelf.logAllTitles();
+
+  //Exercise 6 
+
+  function expect(val) {
+    return {
+      toBe: function (otherVal) {
+        return { "value": val === otherVal };
+      },
+      notToBe: function (otherVal) {
+        return { "value": val !== otherVal };
+      }
+    };
+  }
+  //one
+  const result1 = expect(5).toBe(5);
+  console.log(result1);
+  //two
+  const result2 = expect(5).toBe(null);
+  console.log(result2);
+  //three
+  const result3 = expect(5).notToBe(null);
+  console.log(result3);
+  
+  
+  
+  
+  
+  
+  
+  
